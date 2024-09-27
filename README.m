fd06323600d8201cf063165a23d1c23bf4e063a7
Ludmila Chorvat <119744105+LudmilaChorvat@users.noreply.github.com>
2024-09-27 12:22:44 -0300

    Update README.m
    Program's description

--- README.m
+++ README.m
@@ -1,1 +1,18 @@
-# Data Analysis 2
+# Data Analysis
+# Este programa de gestion de productos esta disenado para almacenar la informacion pertienente a las entradas y salidas de dos tipos de productos: Calzados y Bikinis
+# Fue disenado de esta manera ya que fue realizado con el fin de ser funcional a un emprendimiento dedicado a la venta de estos items
+#
+# Este programa es capaz de realizar las siguientes tareas:
+# Agregar nuevos productos al stock
+# Agregar mas stock al de un producto preexistente
+# Fijar un precio de venta teniendo en cuenta el precio de costo y un incremento porcentual definido por el usuario 
+# Actualizar el precio de productos existentes y con stock disponible 
+# Eliminar productos del stock
+# Filrar productos por codigo o nombre del modelo
+# Mostrar la lista entera de stock disponible 
+# 
+# Algunas recomendaciones:
+# Este programa identifica a los productos en funcion de su codigo, el cual tiene el soqguiente formato:
+# XXX-XX donde los primeros tres numeros corresponden al codigo y los siguientes dos al talle, por ejemplo 
+# Para un calzado: XXX-XX = 001-38
+# Para un bikini: XXX-XX = 002-M
